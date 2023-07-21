a0a3c6b22e3d2baa207f0a5609a7d2bcc43a6ad4
dat-030120 <139488608+dat-030120@users.noreply.github.com>
2023-07-21 17:51:16 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Box from "./component/box";
+import "./styles.css";
+
+export default function App() {
+  const element = [
+    {
+      id: 1,
+      name: "người yêu",
+      description: "chị châu lúc còn cute",
+      image:
+        "./asset/image1/356540439_242671781961185_7403362414090560176_n.jpg"
+      // image: "./asset/image/values-1.png"
+    },
+    {
+      id: 2,
+      name: "người yêu đang chinh",
+      description: "chị châu nhìn đẹp vãi ò",
+      // image: "./asset/image/values-2.png"
+      image:
+        "./asset/image1/356934544_1278821529661103_5289519354125196668_n.jpg"
+    },
+    {
+      id: 3,
+      name: "người yêu đang định ăn tui",
+      description: "ma nữ kìa tránh xa tui ra",
+      // image: "./asset/image/values-3.png"
+      image:
+        "./asset/image1/358337325_1959013004454509_5539916661064496485_n.jpg"
+    }
+  ];
+  const listElement = element.map((item, index) => {
+    return <Box key={item.id} data={index} />;
+  });
+  return (
+    <div className="container-fluid">
+      <div className="row">{listElement}</div>
+    </div>
+  );
+}
